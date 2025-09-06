108ea46ef5a5be41843b932569b72f1a322ac0cf
Benjamin Herbas Miranda <cobubenjamin898@gmail.com>
2025-09-06 11:59:20 -0400

    PRUEBA

--- BACKEND/inmobiliaria/settings.py
+++ BACKEND/inmobiliaria/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'usuario.Usuario'
 
 # Application definition
 
@@ -37,16 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
+    'usuario',
     'rest_framework',
     'rest_framework.authtoken',
-    'Usuarios'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -79,8 +78,12 @@ WSGI_APPLICATION = 'inmobiliaria.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'inmobiliaria', # ponen el nombre de su base de datos de postgresql
+        'USER':'postgres',
+        'PASSWORD':'CObuchan8', # su contraseña que usan para entrar a postgresql
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
@@ -125,5 +128,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000",]
--- BACKEND/usuario/admin.py
+++ BACKEND/usuario/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
 
+#from .models import Rol, Usuario, Cliente, Agente
 # Register your models here.
