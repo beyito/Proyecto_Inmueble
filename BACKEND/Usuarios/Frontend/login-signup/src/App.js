82d58034b43a2cfceb583f364fa3eedd365439f3
KevinAntonio453 <antoniokevin453@gmail.com>
2025-09-13 10:46:01 -0400

    Prueba

---
+++ BACKEND/Usuarios/Frontend/login-signup/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginSignup from "./components/LoginSignup/LoginSignup"; // Ajusta la ruta si tu archivo está en otra carpeta
+import Perfil from "./components/Perfil/Perfil";
+function App() {
+  return (
+    <div>
+      <LoginSignup />
+      <perfil />
+    </div>
+  );
+}
+
+export default App;
